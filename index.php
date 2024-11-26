2865a16cad6eebc027a83c3e2ca461debf6bbb2b
Joppe Van Hoof <joppe.vanhoof@gmail.com>
2024-11-26 10:28:40 +0100

    add index.php with debug setup

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+print '<pre>';
+print_r($_GET);
+print '</pre>';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>What's That Pic!</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
