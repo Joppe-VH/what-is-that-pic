8b26c4cf279829c6f33b84cf8d974f20231c81ca
Joppe Van Hoof <joppe.vanhoof@gmail.com>
2024-11-26 10:38:25 +0100

    add mvp.css

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ print '</pre>';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>What's That Pic!</title>
+    <link rel="stylesheet" href="https://unpkg.com/mvp.css">
 </head>
 
 <body>
